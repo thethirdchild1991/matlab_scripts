45c53f6de5027add399edea91039c81ab72aded8
Denis <denis.dylko@gmail.com>
2017-11-11 14:16:29 +0300

    .

--- img_procs/mlab_script.m
+++ img_procs/mlab_script.m
@@ -2,7 +2,7 @@ clc;
 clear;
 close all;
 
-files = dir('D:/work/GSPV/src/camera-gc-polivektris/bin/x86/release/result/white/*.jpg');
+files = dir('D:/work/GSPV/src/camera-gc-polivektris/bin/x86/release/result/white/special/*.jpg');
 files_num = length(files);
 
 res = zeros(5,files_num);
@@ -11,28 +11,36 @@ for i = 1: files_num
     
     res(1,i) = str2num(cell2mat(m_tmp(2)));%file number
     res(2,i) = str2num(cell2mat(m_tmp(3)));%300-B-G-R
-    res(3,i) = 100-str2num(cell2mat(m_tmp(4)));%B
-    res(4,i) = 100-str2num(cell2mat(m_tmp(5)));%G
-    res(5,i) = 100-str2num(cell2mat(m_tmp(6)));%R
+    res(3,i) = str2num(cell2mat(m_tmp(4)));%B
+    res(4,i) = str2num(cell2mat(m_tmp(5)));%G
+    res(5,i) = str2num(cell2mat(m_tmp(6)));%R
 end
+
+
 
 figure;
 plot(res(1,:),res(2,:),'.');
 grid on;
 title('300-B-G-R');
 
+
 figure;
-plot(res(1,:),res(3,:)./res(2,:),'.');
+plot(res(1,:),res(3,:),'.');
 grid on;
 title('B');
 
 
 figure;
-plot(res(1,:),res(4,:)./res(2,:),'.');
+plot(res(1,:),res(4,:),'.');
 grid on;
 title('G');
 
 figure;
-plot(res(1,:),res(5,:)./res(2,:),'.');
+plot(res(1,:),res(5,:),'.');
 grid on;
 title('R');
+
+figure;
+plot(res(3,:)./res(5,:),res(1,:),'.' );
+grid on;
+title('B / R');
