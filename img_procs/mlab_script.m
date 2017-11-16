15036344f36dd9f80423c93c6f27ae9e9d8551eb
Denis <denis.dylko@gmail.com>
2017-11-16 15:24:57 +0300

    .

--- img_procs/mlab_script.m
+++ img_procs/mlab_script.m
@@ -2,10 +2,10 @@ clc;
 clear;
 close all;
 
-files = dir('D:/work/GSPV/src/camera-gc-polivektris/bin/x86/release/result/white/special/*.jpg');
+files = dir('D:/work/GSPV/src/camera-gc-polivektris/bin/x86/release/result/notWhite/*.jpg');
 files_num = length(files);
 
-res = zeros(5,files_num);
+res = zeros(6,files_num);
 for i = 1: files_num
     m_tmp = strsplit(files(i).name, {'_', '.'});    
     
@@ -14,6 +14,7 @@ for i = 1: files_num
     res(3,i) = str2num(cell2mat(m_tmp(4)));%B
     res(4,i) = str2num(cell2mat(m_tmp(5)));%G
     res(5,i) = str2num(cell2mat(m_tmp(6)));%R
+    res(6,i) = str2num(cell2mat(m_tmp(7)));%MAX VAL
 end
 
 
@@ -41,6 +42,11 @@ grid on;
 title('R');
 
 figure;
-plot(res(3,:)./res(5,:),res(1,:),'.' );
+plot(res(1,:),res(6,:),'.');
+grid on;
+title('MAX VAL');
+
+figure;
+plot(res(1,:),res(3,:)./res(5,:),'.' );
 grid on;
 title('B / R');
