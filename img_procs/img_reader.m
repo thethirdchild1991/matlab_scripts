f405f7744e979e03a5e5187856025cf920c079da
Denis <denis.dylko@gmail.com>
2017-12-11 00:23:59 +0300

    .

---
+++ img_procs/img_reader.m
@@ -0,0 +1,108 @@
+clc;
+clear;
+close all;
+
+do_single_plot = 1;
+path = 'E:\work\ARDEONOVA\REPOS\camera-gs-polivektris\bin\x86\release\result\'
+files = dir([path '*.jpg']);
+files_num = length(files);
+
+if do_single_plot == 1
+    for i = 1:1
+        close all;
+        img = imread( ['E:\work\ARDEONOVA\REPOS\camera-gs-polivektris\bin\x86\release\result\img_5143_green_76.jpg' ]);    
+        hsv = rgb2hsv(img);
+        imshow(img);    
+
+        [Hh Hx] = imhist(hsv(:,:,1));    
+        [Sh Sx] = imhist(hsv(:,:,2));    
+        [Vh Vx] = imhist(hsv(:,:,3));
+
+        figure;
+        subplot(3,1,1);
+        plot(Hx(2:end)*360,Hh(2:end), '-');
+        grid on;
+        title('H hist');
+        subplot(3,1,2);
+        plot(Sx(2:end),Sh(2:end), '.');
+        grid on;
+        title('S hist');
+        subplot(3,1,3);
+        plot(Vx(2:end),Vh(2:end), '.');
+        grid on;
+        title('V hist');
+
+
+        figure;
+        subplot(5,1,1);
+        plot(Hx(2:end)*360,Hh(2:end), '-');
+        grid on;
+        title('H hist');
+
+        subplot(5,1,2);
+        plot(Hx(1:end)*360, medfilt1(Hh(1:end),3), '.');
+        title('H hist MEDIAN');
+        grid on;
+
+        subplot(5,1,3);
+        plot(Hx(1:end)*360, smooth(medfilt1(Hh(1:end),3),3), '.');
+        grid on;
+        title('H smooth');
+
+        subplot(5,1,4);
+        plot(Hx(1:end-1)*360, diff(smooth(medfilt1(Hh(1:end),3),3)));
+        grid on;
+        title('H diff');
+
+        subplot(5,1,5);
+            [peaks loc] = findpeaks(smooth(medfilt1(Hh(1:end),3),3));
+
+        plot(loc, peaks, 'x');
+        grid on;
+        title('H diff 2');
+
+        figure;
+        [X, Y ] = size(hsv(:,:,1));
+        mesh(hsv(:,:,1).*180);
+    end
+else
+    res = zeros(2,files_num);
+    
+    
+    for i = 1:files_num
+        m_tmp = strsplit(files(i).name, {'_', '.'});    
+        files(i).name
+        img = imread( [path files(i).name] );    
+        hsv = rgb2hsv(img);          
+
+        [Hh Hx] = imhist(hsv(:,:,1));    
+        [Sh Sx] = imhist(hsv(:,:,2));    
+        [Vh Vx] = imhist(hsv(:,:,3));
+        
+        Hh_median = medfilt1(Hh(1:end),3);
+        Hh_median_smoothed = smooth(medfilt1(Hh(1:end),3),3);
+        Hh_median_smothed_diff = diff(smooth(medfilt1(Hh(1:end),3),3));
+        [peaks loc] = findpeaks(Hh_median_smoothed);
+        
+%         xcorr
+        
+        [C I] = max(Hh_median_smoothed);
+        test_signal = Hh_median_smoothed(I-10:I+10);
+        sig_diff = test_signal - test_signal(end:-1:1);
+        
+        figure;
+        subplot(2,1,1);
+        plot(Hh_median_smoothed);
+        subplot(2,1,2);
+        plot(sig_diff);
+
+        res(1,i) = str2num(cell2mat(m_tmp(2)));%file number      
+        peaks_num = size(peaks);
+        res(2,i) = peaks_num(1);
+        i
+    end
+    
+    figure;
+    plot(res(1,:),res(2,:), '.');
+    grid on;
+end
--- img_procs/mlab_script.m
+++ img_procs/mlab_script.m
@@ -2,68 +2,127 @@ clc;
 clear;
 close all;
 
-files = dir('D:\work\REPOS\camera-gs-polivektris\bin\x86\release\result\*.jpg');
+files = dir('E:\work\ARDEONOVA\REPOS\camera-gs-polivektris\bin\x86\release\result\*.jpg');
 files_num = length(files);
 
-res = zeros(7,files_num);
+res = zeros(8,files_num);
+B = 2;
+G = 3;
+R = 4;
+
+H = 5;
+S = 6;
+V = 7;
+
+MAX_VAL = 8;
+
 for i = 1: files_num
     m_tmp = strsplit(files(i).name, {'_', '.'});    
-%     res(1,i) = str2num(cell2mat(m_tmp(2)));%file number    
-%     res(2,i) = str2num(cell2mat(m_tmp(end-1)));%SAT to SIZE    
-    res(1,i) = str2num(cell2mat(m_tmp(2)));%file number    
-    res(3,i) = str2num(cell2mat(m_tmp(end - 3)));%B
-    res(4,i) = str2num(cell2mat(m_tmp(end - 2)));%G
-    res(5,i) = str2num(cell2mat(m_tmp(end - 1)));%R
-%     res(6,i) = str2num(cell2mat(m_tmp(6)));%MAX VAL
-    res(7,i) = max(res(3:5,i));
+%     [n m] = size(m_tmp);
+%     if(m > 5)
+%         res(1,i) = str2num(cell2mat(m_tmp(2)));%file number    
+%         if (strcmp(m_tmp(4), 'S'))
+%             res(2,i) = str2num(cell2mat(m_tmp(end-2)));%S
+%         end
+%         if (strcmp(m_tmp(4),'V'))
+%             res(3,i) = str2num(cell2mat(m_tmp(end-2)));%V
+%         end
+%     end
+%     res(3,i) = str2num(cell2mat(m_tmp(end-2)));%H
+    res(1,i) = str2num(cell2mat(m_tmp(2)));%file number 
+    res(2,i) = str2num(cell2mat(m_tmp(end-1)));%S
+%     
+%     res(B,i) = str2num(cell2mat(m_tmp(4)));%B
+%     res(G,i) = str2num(cell2mat(m_tmp(5)));%G
+%     res(R,i) = str2num(cell2mat(m_tmp(6)));%R
+%     
+%     res(H,i) = str2num(cell2mat(m_tmp(8)));%B
+%     res(S,i) = str2num(cell2mat(m_tmp(9)));%G
+%     res(V,i) = str2num(cell2mat(m_tmp(10)));%R
+% %     res(6,i) = str2num(cell2mat(m_tmp(6)));%MAX VAL
+%     res(MAX_VAL,i) = max(res(2:3,i));
 end
 
 
+figure;
+plot(res(1,:),res(2,:),'.');
+grid on;
+title('S val');
+
+% figure;
+% plot(res(1,:),res(3,:),'.');
+% grid on;
+% title('V val');
+
+
+% figure;
+% plot(res(1,:),res(3,:),'.');
+% grid on;
+% title('PIX cnt');
 
 % figure;
 % plot(res(1,:),res(2,:),'.');
 % grid on;
 % title('WHITE AREA TO OBJECT AREA');
 
-
-figure;
-plot(res(1,:),3 - res(3,:)./res(7,:) - res(4,:)./res(7,:) - res(5,:)./res(7,:),'.');
-grid on;
-title('300-B-G-R');
-
-
-figure;
-plot(res(1,:),res(3,:)./res(7,:),'.');
-grid on;
-title('B');
-
-
-figure;
-plot(res(1,:),res(4,:)./res(7,:),'.');
-grid on;
-title('G');
-
-figure;
-plot(res(1,:),res(5,:)./res(7,:),'.');
-grid on;
-title('R');
- 
 % figure;
-% plot(res(1,:),res(6,:),'.');
+% plot(res(1,:),res(B,:) + res(G,:) + res(R,:),'.');
 % grid on;
-% title('MAX VAL');
+% title('B+G+R');
+
+% figure;
+% plot(res(1,:),3 - res(3,:)./res(7,:) - res(4,:)./res(7,:) - res(5,:)./res(7,:),'.');
+% grid on;
+% title('300-B-G-R');
+
+
+% figure;
+% plot(res(1,:),res(3,:)./res(7,:),'.');
+% grid on;
+% title('B');
 % 
-figure;
-plot(res(1,:),res(3,:)./res(5,:),'.' );
-grid on;
-title('B / R');
+% 
+% figure;
+% plot(res(1,:),res(4,:)./res(7,:),'.');
+% grid on;
+% title('G');
+% 
+% figure;
+% plot(res(1,:),res(5,:)./res(7,:),'.');
+% grid on;
+% title('R');
+%  
+% % figure;
+% % plot(res(1,:),res(6,:),'.');
+% % grid on;
+% % title('MAX VAL');
+% % 
+% figure;
+% plot(res(1,:),res(3,:)./res(5,:),'.' );
+% grid on;
+% title('B / R');
+% 
+% figure;
+% plot(res(1,:),res(3,:)./res(4,:),'.' );
+% grid on;
+% title('B / G');
+% 
+% figure;
+% plot(res(1,:),res(5,:)./res(4,:),'.' );
+% grid on;
+% title('R / G');
 
-figure;
-plot(res(1,:),res(3,:)./res(4,:),'.' );
-grid on;
-title('B / G');
-
-figure;
-plot(res(1,:),res(5,:)./res(4,:),'.' );
-grid on;
-title('R / G');
+% figure;
+% plot(res(1,:),res(H,:),'.' );
+% grid on;
+% title('H');
+% 
+% figure;
+% plot(res(1,:),res(S,:),'.' );
+% grid on;
+% title('S');
+% 
+% figure;
+% plot(res(1,:),res(V,:),'.' );
+% grid on;
+% title('V');
